851dc25f4c190f5516723990045cff71302c1352
mfaisalkhatri <mohammadfaisalkhatri@gmail.com>
2022-11-04 12:21:42 +0530

    refactored

--- src/main/java/io/github/mfaisalkhatri/drivers/DriverManager.java
+++ src/main/java/io/github/mfaisalkhatri/drivers/DriverManager.java
@@ -15,6 +15,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 /**
